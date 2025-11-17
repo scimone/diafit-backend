b61e922741f1fa1bcadcdebb5ecdb72a5878eb35
Anna Scimone <56034784+scimone@users.noreply.github.com>
2025-11-17 19:13:37 +0100

    Add fields to settings model

--- accounts/models/usersettings.py
+++ accounts/models/usersettings.py
@@ -7,15 +7,24 @@ class UserSettings(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="settings"
     )
 
+    # System settings
+    timezone = models.CharField(max_length=50, default=settings.TIME_ZONE)
+
+    # Appearance settings
     theme = models.CharField(
         max_length=20, default="dark", choices=[("light", "Light"), ("dark", "Dark")]
     )
 
+    # Diabetes management settings
     preferred_unit = models.CharField(
         max_length=10,
         default="mg/dL",
         choices=[("mg/dL", "mg/dL"), ("mmol/L", "mmol/L")],
     )
+    glucose_target_very_low = models.IntegerField(default=54)
+    glucose_target_low = models.IntegerField(default=70)
+    glucose_target_high = models.IntegerField(default=180)
+    glucose_target_very_high = models.IntegerField(default=250)
 
     def __str__(self):
         return f"Settings for {self.user.username}"
--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import UserSettings
+from accounts.models.usersettings import UserSettings
 
 
 @receiver(post_save, sender=User)
